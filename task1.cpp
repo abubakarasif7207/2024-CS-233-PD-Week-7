31565ab12775d3372c5c2c21578d7e4d647b1cb3
abubakarasif7207 <abubakarprofessional7207@gmail.com>
2024-10-20 06:39:53 -0700

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+void diamond(int rowsize);
+main()
+{
+    int rowsize;
+    cout<<"enter the number of rows ";
+    cin>>rowsize;
+    diamond(rowsize);
+}
+void diamond(int rowsize)
+{
+    for(int i=rowsize;i>=1;i--)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<"*";
+
+        }
+        cout<<endl;
+    }
+}
+
+
